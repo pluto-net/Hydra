49f65052f84c816b890d090fe38ae8bf2efd1fde
Tyler Shin <shincode@icloud.com>
2019-05-05 19:23:39 +0900

    add basic authentication

--- imports/api/tweets/server/publications.js
+++ imports/api/tweets/server/publications.js
@@ -2,13 +2,13 @@ import { Meteor } from "meteor/meteor";
 import { Tweets } from "../tweets.js";
 
 Meteor.publish("tweets.all", function() {
-  // if (this.userId === 'superuser') {
-  return Tweets.find();
-  // } else {
-  // Declare that no data is being published. If you leave this line out,
-  // Meteor will never consider the subscription ready because it thinks
-  // you're using the `added/changed/removed` interface where you have to
-  // explicitly call `this.ready`.
-  // return [];
-  // }
+  if (Meteor.user() && Meteor.user().profile && Meteor.user().profile.admin) {
+    return Tweets.find();
+  } else {
+    // Declare that no data is being published. If you leave this line out,
+    // Meteor will never consider the subscription ready because it thinks
+    // you're using the `added/changed/removed` interface where you have to
+    // explicitly call `this.ready`.
+    return [];
+  }
 });
