d19d26949db03d4b575ae7ca1828cb44bd094156
Tylor Shin <shincode@icloud.com>
2019-05-15 16:47:43 +0900

    change ui to show passed time

--- imports/ui/components/tweetList/tweetList.js
+++ imports/ui/components/tweetList/tweetList.js
@@ -1,6 +1,8 @@
 import { Meteor } from "meteor/meteor";
+import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
 import { Tweets } from "/imports/api/tweets/tweets.js";
 import "./tweetList.html";
+import "./tweetList.scss";
 
 Template.tweetList.onCreated(function() {
   Meteor.subscribe("tweets.all");
@@ -8,6 +10,11 @@ Template.tweetList.onCreated(function() {
 
 Template.tweetList.helpers({
   tweets() {
-    return Tweets.find();
+    const rawTweets = Tweets.find().fetch();
+    const tweets = rawTweets.map(t => ({
+      ...t,
+      createdAt: distanceInWordsToNow(t.createdAt, { addSuffix: true })
+    }));
+    return tweets;
   }
 });
