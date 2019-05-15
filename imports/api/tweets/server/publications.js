861c8b1a8bc5901fb62a3ad2ca9e7054721aa603
Tylor Shin <shincode@icloud.com>
2019-05-15 16:38:31 +0900

    limit showing results to 100

--- imports/api/tweets/server/publications.js
+++ imports/api/tweets/server/publications.js
@@ -3,7 +3,7 @@ import { Tweets } from "../tweets.js";
 
 Meteor.publish("tweets.all", function() {
   if (Meteor.user() && Meteor.user().profile && Meteor.user().profile.admin) {
-    return Tweets.find();
+    return Tweets.find({}, { limit: 100 });
   } else {
     // Declare that no data is being published. If you leave this line out,
     // Meteor will never consider the subscription ready because it thinks
